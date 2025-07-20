dcd1fc6475745bd379aa626fbddfd87ca559bfe7
servaniveeranki <145693727+servaniveeranki@users.noreply.github.com>
2025-07-20 20:23:31 +0530

    latest changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,20 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      },
-      '/auth': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  return {
+    base: './',
+    plugins: [react()],
+    server: mode === 'development' ? {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false
+        },
+        '/auth': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false
+        }
       }
-    }
+    } : undefined
   }
 })
